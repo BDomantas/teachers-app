783ceb940d7c719aeebc57408f7daf1a7a23508a
DomantasB <domantas@cct.lt>
2020-08-15 04:50:56 +0300

    wip

--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -2,73 +2,42 @@ import React from "react";
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar
 } from "react-native";
-import Header from "@Components/Header";
 
-const HomeScreen: React.FC<{}>= () => {
+interface HomeScreenProps {
+  userName: string;
+}
+
+const HomeScreen: React.FC<HomeScreenProps>= ({ userName = 'Alex'}) => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          <Header />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>testas</Text>
-            </View>
-          </View>
-          <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>This is 3423434</Text>
-          </View>
-        </ScrollView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>Labas {userName}, </Text>
+          <Text style={styles.subtitleText}>Pasirink ką nori išmokti </Text>
+        </View>
       </SafeAreaView>
-    </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: "white"
+  container: {
+    marginHorizontal: 24,
+    flex: 1,
   },
-  engine: {
-    position: "absolute",
-    right: 0
+  headingText: {
+    fontSize: 28,
   },
-  body: {
-    backgroundColor: "white"
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
+  subtitleText: {
+    paddingTop: 16,
     fontSize: 24,
-    fontWeight: "600",
-    color: "black"
+    color: '#61688B'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: "black"
-  },
-  highlight: {
-    fontWeight: "700"
-  },
-  footer: {
-    color: "gray",
-    fontSize: 12,
-    fontWeight: "600",
-    padding: 4,
-    paddingRight: 12,
-    textAlign: "right"
+  headingContainer: {
+    justifyContent: 'flex-start',
+    paddingTop: 60,
+    flex: 1,
   }
 });
 
