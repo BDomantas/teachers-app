a079a7ba5cc30e1905045bc10247a265b9b135ac
DomantasB <domantas@cct.lt>
2020-08-19 22:38:11 +0300

    improve list to masonry list

--- src/components/CourseCard/index.tsx
+++ src/components/CourseCard/index.tsx
@@ -1,50 +1,72 @@
-import React from 'react';
-import { Text, ImageBackground, ImageSourcePropType, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import {
+  Text,
+  ImageBackground,
+  ImageSourcePropType,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import _ from "lodash";
 
 interface CourseCardProps {
-    title: string;
-    courseCount: number;
-    image: ImageSourcePropType;
-    id: string;
-    onPress: any;
-    index: number;
+  title: string;
+  courseCount: number;
+  image: ImageSourcePropType;
+  id: string;
+  onPress: any;
+  width: number;
 }
 
-const CourseCard: React.FC<CourseCardProps> = ({ title, courseCount, image, onPress }) => {
-    return (
-        <TouchableOpacity style={styles.container } onPress={onPress}>
-            <ImageBackground
-                source={image}
-                style={[styles.image ]}
-                imageStyle={{borderRadius: 10}}
-            >
-                <Text style={styles.titleText}> {title} </Text>
-                <Text style={styles.coursesText}> {courseCount} Courses </Text>
-            </ImageBackground>
-        </TouchableOpacity>
-
-    );
+const CourseCard: React.FC<CourseCardProps> = ({
+  title,
+  courseCount,
+  image,
+  onPress,
+  width,
+}) => {
+  const aspectRatio = _.random(80, 150);
+  console.log(width, aspectRatio);
+  return (
+    <TouchableOpacity
+      style={[
+        styles.container,
+        { height: width * 1.5 * (aspectRatio / 145), width },
+      ]}
+      onPress={onPress}
+    >
+      <ImageBackground
+        source={image}
+        style={styles.imageBackground}
+        imageStyle={styles.imageBackgroundImage}
+      >
+        <Text style={styles.titleText}> {title} </Text>
+        <Text style={styles.coursesText}> {courseCount} Courses </Text>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'transparent',
-    },
-    titleText: {
-        fontSize: 18,
-        color: '#0D1333'
-    },
-    coursesText: {
-        fontSize: 14,
-        color: '#8F95B3',
-    },
-    image: {
-        flex: 1,
-        padding: 15,
-        margin: 5,
-        aspectRatio: 0.71/1
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "transparent",
+  },
+  titleText: {
+    fontSize: 18,
+    color: "#0D1333",
+  },
+  coursesText: {
+    fontSize: 14,
+    color: "#8F95B3",
+  },
+  imageBackground: {
+    flex: 1,
+    marginBottom: 10,
+    padding: 10,
+  },
+  imageBackgroundImage: {
+    borderRadius: 10,
+  },
 });
 
 export default CourseCard;
--- src/components/CourseList/index.tsx
+++ src/components/CourseList/index.tsx
@@ -1,63 +1,116 @@
-import React from 'react';
-import { View, Text, FlatList, StyleSheet, ImageSourcePropType } from 'react-native';
-import CourseCard from '@Components/CourseCard';
-import Input from '@Components/Input';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageSourcePropType,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import CourseCard from "@Components/CourseCard";
+import Input from "@Components/Input";
 
 interface CourseListData {
-    title: string;
-    courseCount: number;
-    id: string;
-    image: ImageSourcePropType
+  title: string;
+  courseCount: number;
+  id: string;
+  image: ImageSourcePropType;
 }
 
 const MOCK_DATA: Array<CourseListData> = [
-    { title: "Math", courseCount: 1, id: "Math", image: require('../../assets/images/courseCard1.png') },
-    { title: "English", courseCount: 2, id: "English", image: require('../../assets/images/courseCard2.png') },
-    { title: "Programming", courseCount: 3, id: "Programming", image: require('../../assets/images/courseCard3.png')},
-    { title: "Business", courseCount: 1, id: "Business", image: require('../../assets/images/courseCard4.png') }
-]
+  {
+    title: "Math",
+    courseCount: 1,
+    id: "Math",
+    image: require("../../assets/images/courseCard1.png"),
+  },
+  {
+    title: "English",
+    courseCount: 2,
+    id: "English",
+    image: require("../../assets/images/courseCard2.png"),
+  },
+  {
+    title: "Programming",
+    courseCount: 3,
+    id: "Programming",
+    image: require("../../assets/images/courseCard3.png"),
+  },
+  {
+    title: "Business",
+    courseCount: 1,
+    id: "Business",
+    image: require("../../assets/images/courseCard4.png"),
+  },
+];
 
+const { width: wWidth } = Dimensions.get("window");
 
 const CourseList = () => {
-    const onCourseCardPress = (id) => {
-        console.log("Pressed on ", id);
-    };
-    const renderItem = ({ item, index }) => {
-        return (
-            <CourseCard {...item} index={index} onPress={() => onCourseCardPress(item.id)}/>
-        );
-    }
-    return (
-        <View style={styles.container}>
-            <View style={styles.inputContainer}>
-                <Input placeholder={'Search'} />
-            </View>
-            <Text style={styles.listTitleText}>Courses</Text>
-            <FlatList
-                numColumns={2}
-                data={MOCK_DATA}
-                keyExtractor={({ id }) => id}
-                renderItem={renderItem}
-            >
-            </FlatList>
+  const cardWidth = (wWidth - 24 * 2 - 10) / 2;
+
+  const onCourseCardPress = (id) => {
+    console.log("Pressed on ", id);
+  };
+
+  const renderItems = (divider: number) => (list: Array<CourseListData>) =>
+    list
+      .filter((_, index) => index % 2 === divider)
+      .map((item) => (
+        <CourseCard
+          {...item}
+          width={cardWidth}
+          key={item.id}
+          onPress={() => onCourseCardPress(item.id)}
+        />
+      ));
+
+  const renderLeft = renderItems(0);
+  const renderRight = renderItems(1);
+
+  // 24 is supposed to reference horizontal margin in HomeScreen.tsx
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Input placeholder={"Search"} />
+      </View>
+      <Text style={styles.listTitleText}>Courses</Text>
+      <ScrollView>
+        <View style={styles.scrollContentContainer}>
+          <View style={[styles.masonryRow, styles.masonryRowLeft]}>
+            {renderLeft(MOCK_DATA)}
+          </View>
+          <View style={styles.masonryRow}>{renderRight(MOCK_DATA)}</View>
         </View>
-    );
-}
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    listTitleText: {
-        color: '#0D1333',
-        fontSize: 20,
-        paddingBottom: 24,
-        fontWeight: 'bold'
-    },
-    inputContainer: {
-        paddingBottom: 48,
-        paddingTop: 24
-    }
+  container: {
+    flex: 1,
+  },
+  listTitleText: {
+    color: "#0D1333",
+    fontSize: 20,
+    paddingBottom: 24,
+    fontWeight: "bold",
+  },
+  inputContainer: {
+    paddingBottom: 48,
+    paddingTop: 24,
+  },
+  masonryRow: {
+    flex: 1,
+  },
+  masonryRowLeft: {
+    marginRight: 10,
+  },
+  scrollContentContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
 });
 
 export default CourseList;
